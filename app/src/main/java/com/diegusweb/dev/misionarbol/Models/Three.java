92c6cae493324cbde84f8627abce14ec922ba5a4
Diego Rueda <diego.rueda2@gmail.com>
2017-01-05 15:57:59 -0400

    add list from Biblioteca tab

---
+++ app/src/main/java/com/diegusweb/dev/misionarbol/Models/Three.java
@@ -0,0 +1,54 @@
+package com.diegusweb.dev.misionarbol.models;
+
+/**
+ * Created by HP on 05/01/2017.
+ */
+
+public class Three {
+
+    private int id;
+    private String name;
+    private String description;
+    private int idDrawable;
+
+    public Three(String description, int id, int idDrawable, String name) {
+        this.description = description;
+        this.id = id;
+        this.idDrawable = idDrawable;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdDrawable() {
+        return idDrawable;
+    }
+
+    public void setIdDrawable(int idDrawable) {
+        this.idDrawable = idDrawable;
+    }
+
+
+}
--- app/src/main/java/com/diegusweb/dev/misionarbol/fragments/ThreeLibraryFragment.java
+++ app/src/main/java/com/diegusweb/dev/misionarbol/fragments/ThreeLibraryFragment.java
@@ -1,19 +1,33 @@
 package com.diegusweb.dev.misionarbol.fragments;
 
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.diegusweb.dev.misionarbol.MainActivity;
 import com.diegusweb.dev.misionarbol.R;
+import com.diegusweb.dev.misionarbol.activity.RouteActivity;
+import com.diegusweb.dev.misionarbol.adapter.AdaptadorMenu;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ThreeLibraryFragment extends Fragment {
 
+    private RecyclerView reciclador;
+    private GridLayoutManager layoutManager;
+    private AdaptadorMenu adaptador;
 
     public ThreeLibraryFragment() {
         // Required empty public constructor
@@ -21,10 +35,51 @@ public class ThreeLibraryFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_three_library, container, false);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        adaptador = new AdaptadorMenu(getActivity());
+
+        adaptador.setOnItemClickListener(new AdaptadorMenu.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(View itemView, int position) {
+                String name = adaptador.getItem(position).getNombre();
+                int id = adaptador.getItem(position).getId();
+                Toast.makeText(getActivity(),  "Arbol was clicked! " + name, Toast.LENGTH_SHORT).show();
+
+                Intent i = new Intent(getActivity(), RouteActivity.class);
+                i.putExtra("STRING_I_NEED", id);
+
+
+                //startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        if(activity instanceof MainActivity)
+            Log.i("Si se ejecuto", "Yes");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_three_library, container, false);
+
+        reciclador = (RecyclerView) root.findViewById(R.id.recicladorThree);
+        setupArtistList();
+        // setDummyContent();
+        return root;
+    }
+
+    private void setupArtistList(){
+        reciclador.setLayoutManager(new LinearLayoutManager(getActivity()));
+        //poniendo adaptador
+        reciclador.setAdapter(adaptador);
     }
 
 }
