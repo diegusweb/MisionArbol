712ba94d319ed1f53ffff0bacf67ced6117fe251
Diego Rueda <diego.rueda2@gmail.com>
2017-01-09 18:30:30 -0400

    new files from Login

---
+++ app/src/main/java/com/diegusweb/dev/misionarbol/activity/login/ResetPasswordActivity.java
@@ -0,0 +1,15 @@
+package com.diegusweb.dev.misionarbol.activity.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.diegusweb.dev.misionarbol.R;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
