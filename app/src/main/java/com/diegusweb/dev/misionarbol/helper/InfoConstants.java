f4863236847d4bbf74d3bb0b31ea1c5d5c3b4843
Diego Rueda <diego.rueda2@gmail.com>
2017-02-02 22:18:36 -0400

    test api cll retrofit

--- app/src/main/java/com/diegusweb/dev/misionarbol/helper/InfoConstants.java
+++ app/src/main/java/com/diegusweb/dev/misionarbol/helper/InfoConstants.java
@@ -7,6 +7,8 @@ package com.diegusweb.dev.misionarbol.helper;
 public class InfoConstants {
     public static final String BASE_URL = "http://10.0.0.11:8075/2017/laravel/misionarbol/public/api/";
 
+    public static final String BASE_URL_TEST = "https://api.github.com";
+
     public static final String PATH_VERSION = "/2.0";
 
     public static String API_TOKEN = "";
